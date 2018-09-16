5c264f01eb655bd4a93a0a0e2e4f5775f72ec415
Jaume Moral <jaumem@fib.upc.edu>
2018-09-16 16:29:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+pip install -r requirements.txt
+python generar_horaris_sitges.py
+```
+
+Aixo genera 5 fitxers ics per cadascuna de les 5 sales on es projecten pelicules
---
+++ generar_horaris_sitges.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import urllib
+from bs4 import BeautifulSoup
+from icalendar import Calendar, Event
+from pytz import timezone
+from datetime import datetime, timedelta
+
+link = "https://sitgesfilmfestival.com/cat/programa"
+f = urllib.urlopen(link)
+html_doc=open("programa","r").read()
+soup = BeautifulSoup(html_doc, 'html.parser')
+f.close()
+	
+cal = {}
+cal['Prado']=Calendar()
+cal['Retiro']=Calendar()
+cal['Auditori']=Calendar()
+cal['Tramuntana']=Calendar()
+cal['Brigadoon']=Calendar()
+lt = timezone('Europe/Paris')
+
+taula=soup.find("table")
+files=taula.find_all("tr")
+for fila in files:
+  tds=fila.find_all("td")
+  if len(tds)>=4:
+    hora=tds[1].text.strip().replace("\n\n"," ")
+    inici=datetime.strptime(hora,"%d-%m-%Y %H:%M")
+    peli=tds[2].text.strip().replace("\n","/")
+    lloc=tds[3].text.strip().split()[0]
+    durada=tds[5].text.strip().replace("\'","")
+    if len(durada)==0: durada=120
+    fi=inici+timedelta(minutes=int(durada))
+
+    event = Event()        
+    event.add('summary', peli)
+    event.add('dtstart', inici.replace(tzinfo=lt))
+    event.add('dtend', fi.replace(tzinfo=lt))
+    event.add('location', lloc)
+
+    cal[lloc].add_component(event)
+
+for lloc in cal.keys(): 
+  f = open(lloc+'.ics', 'wb')
+  f.write(cal[lloc].to_ical())
+  f.close()
